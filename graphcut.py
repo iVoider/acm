731a903abf9d5b8055505233ee4934a7e049346a
Alex Dorofeev <write2me4help@gmail.com>
2023-09-29 15:30:15 +0300

    Update graphcut.py

--- graphcut.py
+++ graphcut.py
@@ -1,7 +1,9 @@
-N = 5
+N = 30
 M = N * 4
 
-for _ in range(0, 100):
+rng = np.random.default_rng()
+
+for _ in range(0, 1):
   f = gen_sat(N, M, random_cnf(N))
   g, _ = sat_to_clique(f)
   G = ig.Graph.from_networkx(g)
@@ -10,27 +12,37 @@ for _ in range(0, 100):
 
   res = dict([(v, list()) for v in G.vs.indices])
 
+  all_probs = dict()
+  all_cat = dict()
+
   for v in G.vs.indices:
-    local = set(G.neighbors(v) + [v])
-    global_list = list()
-    for _ in range(0, M ** 2):
+    nv = G.neighbors(v)
+    pv = [ap[x].real for x in nv]
+    pv.append(1.0 - sum(pv))
+    all_probs[v] = pv
+    nv.append(v)
+    all_cat[v] = nv
+  
+  # all probs has some logic error unlike previous version
+  for v in G.vs.indices:
+    local = set(all_cat[v])
+    global_vals = dict.fromkeys(G.vs.indices, 0)
+    for _ in range(0, N * M):
      cur = v
-     while True:
-      rng = np.random.default_rng()
-      probs = [ap[x].real for x in G.neighbors(cur)]
-      probs.append(1.0 - sum(probs))
-      next = np.random.choice(G.neighbors(cur) + [cur], p=probs)
+     steps = 0
+     while steps < N * M:
+      next = random.choices(all_cat[cur], k = 1, weights=all_probs[cur])[0]
       if next in local:
         cur = next
-        global_list.append(cur)
+        global_vals[cur] += 1
       else:
         break
-    r = dict(Counter(global_list).most_common())
-    res[v] = r
-  
+      steps += 1
+    res[v] = global_vals
+
   found = False
   for v in G.vs.indices:
-    sub = set(G.neighbors(v) + [v])
+    sub = set(all_cat[v])
     cur = set({v})
     while len(sub) > M:
      val = dict.fromkeys(sub - cur, 0)
@@ -40,10 +52,12 @@ for _ in range(0, 100):
             val[n] += res[z][n]
      mx = max(val, key=val.get)
      cur.add(mx)
-     sub = set(G.neighbors(v) + [v])
+     sub = set(all_cat[v])
      for c in cur:
-      sub &= set(G.neighbors(c) + [c])
-    if G.subgraph(sub).density() == 1.0 and G.subgraph(sub).vcount() == M:
+      sub &= set(all_cat[c])
+    h = G.subgraph(sub)
+    if h.vcount() == M and h.density() == 1.0:
       found = True
       break
-  print(found)
+  if not found:
+     print('Shit')
