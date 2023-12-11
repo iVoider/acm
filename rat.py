dbe41635e27675dde70e396cd46987cf1184a0bb
Alex Dorofeev <write2me4help@gmail.com>
2023-12-11 14:23:03 +0300

    Update rat.py

--- rat.py
+++ rat.py
@@ -1,50 +1,39 @@
 def aprob(Gr):
-    transition = []
-    for v in Gr.vs:
-        nxt = [0.0] * Gr.vcount()
-        vl = 1.0 / len(v.neighbors())
-        for n in v.neighbors():
-            nxt[n.index] = vl
-        transition.append(nxt)
+  stationary_distrib = list()
+  for v in Gr.vs:
+    stationary_distrib.append(Gr.degree(v) / (2 * G.ecount()))
+  return stationary_distrib
 
-    transition_matrix = np.array(transition)
-
-    transition_matrix_transp = transition_matrix.T
-    eigenvals, eigenvects = np.linalg.eig(transition_matrix_transp)
-    close_to_1_idx = np.isclose(eigenvals, 1)
-    target_eigenvect = eigenvects[:, close_to_1_idx]
-    target_eigenvect = target_eigenvect[:, 0]
-    stationary_distrib = target_eigenvect / sum(target_eigenvect)
-    return stationary_distrib
-
-N = 100
-M = N * 4
-
-for _ in range(0, 1):
-  f = gen_sat(N, M, random_cnf(N))
-  g, _ = sat_to_clique(f)
-  G = ig.Graph.from_networkx(g)
-
+def recalc(G):
   ap = aprob(G)
 
-  res = dict([(v, list()) for v in G.vs.indices])
+  res = dict([(v["_nx_name"], list()) for v in G.vs])
 
   all_sat = dict()
 
-  for v in G.vs.indices:
-    nv = G.neighbors(v)
-    nv.append(v)
-    all_sat[v] = set(nv)
+  for v in G.vs:
+    nv = [G.vs[n]["_nx_name"] for n in G.neighbors(v)]
+    nv.append(v["_nx_name"])
+    all_sat[v["_nx_name"]] = set(nv)
 
+  prios = dict()
   for v in G.vs.indices:
     global_vals = dict()
-    z = sum([ap[x].real for x in G.neighbors(v)])
-    for n in G.neighbors(v):
-      global_vals[n] = ap[n].real / z
-    res[v] = global_vals
 
-  mxs = set()
-  for v in G.vs.indices:
+    z = sum([ap[x] for x in G.neighbors(v)])
+    for n in G.neighbors(v):
+      global_vals[G.vs[n]["_nx_name"]] = ap[n] / z
+    res[G.vs[v]["_nx_name"]] = global_vals
+    prios[v] = z + ap[v].real
+  return sorted(prios, key=prios.get, reverse = True), all_sat, res
+
+def sald(G):
+  orig = dict([(v["_nx_name"], v.index) for v in G.vs])
+
+  p, all_sat0, res0 = recalc(G)
+  for vp in p:
+    all_sat, res = all_sat0, res0
+    v = G.vs[vp]["_nx_name"]
     cur = all_sat[v]
     choice = set({v})
     while len(cur) > M:
@@ -55,6 +44,9 @@ for _ in range(0, 1):
       m = max(mx, key=mx.get)
       choice.add(m)
       cur &= all_sat[m]
-    mxs.add(G.subgraph(cur).clique_number())
-    print(max(mxs), M)
-    break
+      # _, all_sat, res = recalc(G.subgraph([orig[c] for c in cur]))
+    zoo = G.subgraph([orig[c] for c in cur]).clique_number()
+    print(zoo)
+    if zoo == M:
+      print("Booba")
+      break
