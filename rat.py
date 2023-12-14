7c3c31e981be57466075bad15e1d4ff7e553acf8
Alex Dorofeev <write2me4help@gmail.com>
2023-12-14 15:41:10 +0300

    Update rat.py

--- rat.py
+++ rat.py
@@ -4,49 +4,34 @@ def aprob(Gr):
     stationary_distrib.append(Gr.degree(v) / (2 * G.ecount()))
   return stationary_distrib
 
-def recalc(G):
-  ap = aprob(G)
+def bry(G):
+  best = dict()
+  H = G.copy()
+  apn = aprob(G)
 
-  res = dict([(v["_nx_name"], list()) for v in G.vs])
-
-  all_sat = dict()
-
-  for v in G.vs:
-    nv = [G.vs[n]["_nx_name"] for n in G.neighbors(v)]
-    nv.append(v["_nx_name"])
-    all_sat[v["_nx_name"]] = set(nv)
-
-  prios = dict()
   for v in G.vs.indices:
-    global_vals = dict()
+      best[v] = (sum([apn[i] for i in G.neighbors(v)]) + apn[v])
 
-    z = sum([ap[x] for x in G.neighbors(v)])
-    for n in G.neighbors(v):
-      global_vals[G.vs[n]["_nx_name"]] = ap[n] / z
-    res[G.vs[v]["_nx_name"]] = global_vals
-    prios[v] = z + ap[v].real
-  return sorted(prios, key=prios.get, reverse = True), all_sat, res
-
-def sald(G):
-  orig = dict([(v["_nx_name"], v.index) for v in G.vs])
-
-  p, all_sat0, res0 = recalc(G)
-  for vp in p:
-    all_sat, res = all_sat0, res0
-    v = G.vs[vp]["_nx_name"]
-    cur = all_sat[v]
+  for v in sorted(best, key = best.get, reverse=True):
+    ap = apn.copy()
+    G = H.copy()
+    cur = set(G.neighbors(v))
+    cur.add(v)
     choice = set({v})
     while len(cur) > M:
       mx = dict.fromkeys(cur - choice, 0)
-      for x, y in itertools.product(cur - choice, choice):
-         if y in res:
-            mx[x] += res[y][x]
+      for x in cur - choice:
+            mx[x] = (sum([ap[i] for i in G.neighbors(x)]) + ap[x])
       m = max(mx, key=mx.get)
       choice.add(m)
-      cur &= all_sat[m]
-      # _, all_sat, res = recalc(G.subgraph([orig[c] for c in cur]))
-    zoo = G.subgraph([orig[c] for c in cur]).clique_number()
-    print(zoo)
-    if zoo == M:
-      print("Booba")
-      break
+      inc = set(G.neighbors(m))
+      inc.add(m)
+      cur &= inc
+      todel = set()
+      for c in set(G.vs.indices) - cur:
+          todel |= set(G.vs[c].incident())
+      G.delete_edges(todel)
+      ap = aprob(G)
+
+    print(G.subgraph(cur).clique_number(), M)
+    break
