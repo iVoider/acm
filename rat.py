2d1f51e12363e11756d168b82658269b24a94135
Alex Dorofeev <write2me4help@gmail.com>
2023-12-17 01:09:28 +0300

    Update rat.py

--- rat.py
+++ rat.py
@@ -9,8 +9,10 @@ def bry(G):
   H = G.copy()
   apn = aprob(G)
 
+  fnd = False
+
   for v in G.vs.indices:
-      best[v] = (sum([apn[i] for i in G.neighbors(v)]) + apn[v])
+      best[v] = (apn[v], sum([apn[i] for i in G.neighbors(v)]))
 
   for v in sorted(best, key = best.get, reverse=True):
     ap = apn.copy()
@@ -21,13 +23,15 @@ def bry(G):
     for zi in range(1, M + 1):
       mx = dict.fromkeys(cur, 0)
       for x in cur:
-            mx[x] = (sum([ap[i] for i in G.neighbors(x)]) + ap[x])
+            mx[x] = (ap[x], sum([ap[i] for i in G.neighbors(x)]))
       choice = set(sorted(mx, key=mx.get, reverse=True)[0:zi])
       inc = cur.copy()
       for c in choice:
         p = set(G.neighbors(c))
         p.add(c)
         inc &= p
+        if len(inc) < len(cur):
+           break
 
       cur = inc.copy()
 
@@ -36,7 +40,19 @@ def bry(G):
           todel |= set(G.incident(x))
 
       G.delete_edges(todel)
-      ap = aprob(G)
+      while True:
+        changed = False
+        for v in G.vs.indices:
+          if G.degree(v) < (M - 1) and G.degree(v) > 0:
+             G.delete_edges(G.incident(v))
+             changed = True
+        if not changed:
+           break
 
-    print(G.subgraph(choice).clique_number(), M)
-    break
+      ap = aprob(G)
+    t = G.subgraph(choice).clique_number()
+    if t == M:
+       fnd = True
+       break
+
+  assert(fnd)
